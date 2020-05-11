8ef0f7d879a254738c410233b7b6668184f8c4cc
Markus Jotter <markus@jotter.name>
2020-05-11 07:02:59 +0200

    undo changes

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -22,8 +22,6 @@ class BluetoothTerminal {
     this._boundHandleCharacteristicValueChanged =
         this._handleCharacteristicValueChanged.bind(this);
 
-    serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-    characteristicUuid  = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
     // Configure with specified parameters.
     this.setServiceUuid(serviceUuid);
     this.setCharacteristicUuid(characteristicUuid);
@@ -131,7 +129,6 @@ class BluetoothTerminal {
    */
   receive(data) {
     // Handle incoming data.
-     
   }
 
   /**
@@ -245,26 +242,6 @@ class BluetoothTerminal {
     this._log('Requesting bluetooth device...');
 
     return navigator.bluetooth.requestDevice({
-      filters: [{
-        name: 'ESP32'
-      }],
-      optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e',
-      '6e400002-b5a3-f393-e0a9-e50e24dcca9e',
-      '6e400003-b5a3-f393-e0a9-e50e24dcca9e']
-    }).
-        then((device) => {
-          this._log('"' + device.name + '" bluetooth device selected');
-
-          this._device = device; // Remember device.
-          this._device.addEventListener('gattserverdisconnected',
-              this._boundHandleDisconnection);
-
-          return this._device;
-        });
-
-
-    /*
-    return navigator.bluetooth.requestDevice({
       filters: [{services: [this._serviceUuid]}],
     }).
         then((device) => {
@@ -276,7 +253,6 @@ class BluetoothTerminal {
 
           return this._device;
         });
-   */ 
   }
 
   /**
@@ -300,7 +276,7 @@ class BluetoothTerminal {
           return server.getPrimaryService(this._serviceUuid);
         }).
         then((service) => {
-          this._log('Service found', 'Getting characteristic...' + this._characteristicUuid);
+          this._log('Service found', 'Getting characteristic...');
 
           return service.getCharacteristic(this._characteristicUuid);
         }).
